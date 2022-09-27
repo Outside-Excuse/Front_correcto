3a0ac3ee08b17745371438184265d09d3e0f77d2
Alejandro Hidalgo Badillo <alejandrohidalgobadillo@gmail.com>
2022-09-27 12:23:47 -0500

    Initial Commit

---
+++ AppMaravillas/AppMaravillasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMaravillasApp.swift
+//  AppMaravillas
+//
+//  Created by Alejandro Hidalgo Badillo on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AppMaravillasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
