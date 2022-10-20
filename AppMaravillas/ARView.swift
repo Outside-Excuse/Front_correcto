c37989319cf518d51fe31e897949ed0cfe80bc8a
CarlosMilano <a01383102@itesm.mx>
2022-10-20 18:19:49 -0500

    .

--- AppMaravillas/ARView.swift
+++ AppMaravillas/ARView.swift
@@ -11,6 +11,7 @@ import AVFoundation
 struct ARView: View {
     let captureSession = AVCaptureSession()
     var body: some View {
+        Button(action: <#T##() -> Void#>, label: <#T##() -> View#>)
         CameraView()
        
     }
